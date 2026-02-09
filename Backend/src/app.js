31fa3ccd4d9cd9e55a708db2f76a0f16ec01b02f
awadi99 <adityawaghmare9990@gmail.com>
2026-02-09 17:57:36 +0530

    CORS Configured Properly

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,13 +3,23 @@ import cors from "cors";
 import helmet from "helmet";
 import authRoutes from "./routers/auth.route.js";
 import cookieParser from "cookie-parser";
+import dotevn from "dotenv"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename =fileURLToPath(import.meta.url);
+const __dirname =path.dirname(__filename);
+
+dotevn.config({path:path.resolve(__dirname,"../.env")});
+
 
 const app = express();
-app.use(helmet());
+
 app.use(cors({
     origin:process.env.CLIENT_URL,
     credentials:true
 }));
+app.use(helmet());
 app.use(express.json({limit:"25mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
